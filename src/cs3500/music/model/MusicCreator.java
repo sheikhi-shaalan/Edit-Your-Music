c0b12ba4b402324b6e0b80fffdf288fdd9506107
NadineShaalan <shaalann@bxscience.edu>
2016-06-13 17:29:21 -0400

    Notes

--- src/cs3500/music/model/MusicCreator.java
+++ src/cs3500/music/model/MusicCreator.java
@@ -5,32 +5,32 @@ import java.util.List;
 public interface MusicCreator {
   /**
    * Adds a single note to a piece of music
-   * @param note
+   * @param note Note to be added into the Composition
    */
   void addNote(Note note);
 
   /**
    * Removes a single note from a piece of music
-   * @param note
+   * @param note Note to be removed from the Composition
    */
   void removeNote(Note note);
 
   /**
    * Changes a specified note to a newly specified note
-   * @param original
-   * @param newNote
+   * @param original Note be changed
+   * @param newNote Note to change it to
    */
   void changeNote(Note original, Note newNote);
 
   /**
    * Combines this piece with that peice so that they play simultaneously
-   * @param c2
+   * @param c2 piece to play interwoven with this
    */
   void combineSym(MusicCreator c2);
 
   /**
    * Combines this piece so that peice plays right after
-   * @param c2
+   * @param c2 piece to play after this
    */
   void combineCon(MusicCreator c2);
 
@@ -51,11 +51,11 @@ public interface MusicCreator {
    *
    * @return the length of a song
    */
-  public int getSongDuration();
+  int getSongDuration();
 
   /**
-   * @param beat
-   * @returns notes at given beat
+   * @param beat beat number of the desired notes
+   * @return notes at given beat
    */
-  public List<Note> notesAtBeat(int beat);
+  List<Note> notesAtBeat(int beat);
 }
