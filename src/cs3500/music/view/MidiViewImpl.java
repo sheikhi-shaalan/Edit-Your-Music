6ff5c9dcdc194671e84db388055bd33e5953250f
NadineShaalan <shaalann@bxscience.edu>
2016-06-15 17:28:26 -0400

    changed things to private

--- src/cs3500/music/view/MidiViewImpl.java
+++ src/cs3500/music/view/MidiViewImpl.java
@@ -16,6 +16,8 @@ import cs3500.music.model.Note;
 public class MidiViewImpl implements IView {
   private final Synthesizer synth;
   private final Receiver receiver;
+  private final int ONE_BEAT_COEFF = 1000000;
+  private final int SLEEP_NUMBER = 1000;
 
   public MidiViewImpl() {
     Synthesizer temps = null;
@@ -48,42 +50,62 @@ public class MidiViewImpl implements IView {
    * @see <a href="https://en.wikipedia.org/wiki/General_MIDI"> https://en.wikipedia.org/wiki/General_MIDI
    * </a>
    */
-  public void playComposition(MusicCreator c) {
+  public void playComposition(MusicCreator c) throws InvalidMidiDataException {
     for (int i = 0; i <= c.getSongDuration(); i++) {
       try {
         playBeat((ArrayList<Note>) c.notesAtBeat(i));
-
       } catch (InvalidMidiDataException e) {
         e.getStackTrace();
+      }
+
+      try {
+        Thread.sleep(SLEEP_NUMBER);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
       }
     }
     this.receiver.close();
   }
 
-  public void playBeat(ArrayList<Note> list) throws InvalidMidiDataException {
+  private void playBeat(ArrayList<Note> list) throws InvalidMidiDataException {
     for (Note n : list) {
       //TODO Uncouple this (DIVORCE)
-      this.receiver.send(new ShortMessage(ShortMessage.NOTE_ON, 1, n.getKeyVal(), 64), -1);
-      try {
-        Thread.sleep(Integer.MAX_VALUE);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-      this.receiver.send(new ShortMessage(ShortMessage.NOTE_OFF, 1, n.getKeyVal(), 64)
-              , this.synth.getMicrosecondPosition() +  1000000);
+      ShortMessage start = new ShortMessage(ShortMessage.NOTE_ON, 0, n.getKeyVal(), 64);
+      ShortMessage end = new ShortMessage(ShortMessage.NOTE_OFF, 0, n.getKeyVal(), 64);
+      this.receiver.send(start, -1);
+      this.receiver.send(end
+              , this.synth.getMicrosecondPosition() + (n.getDuration() * ONE_BEAT_COEFF));
     }
+
+
   }
 
+  private void playNote() throws InvalidMidiDataException {
+    MidiMessage start = new ShortMessage(ShortMessage.NOTE_ON, 0, 60, 64);
+    //MidiMessage changeNote = new ShortMessage(ShortMessage.PROGRAM_CHANGE, 0, 28);
+    MidiMessage stop = new ShortMessage(ShortMessage.NOTE_OFF, 0, 60, 64);
+    this.receiver.send(start, -1);
+    //this.receiver.send(changeNote, this.synth.getMicrosecondPosition() + 5000);
+    this.receiver.send(stop, this.synth.getMicrosecondPosition() + 1000000);
+    // TODO CHANGE THIS TO A JAVA TIMER
+    try {
+      Thread.sleep(1000);
 
-  public static void main(String[] args) {
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    this.receiver.close(); // Only call this once you're done playing *all* notes
+  }
+
+  public static void main(String[] args) throws InvalidMidiDataException {
     MidiViewImpl m = new MidiViewImpl();
     MusicCreator c = new MusicCreatorImpl();
     c.addNote(new Note(0, Note.Pitch.B, 1, 5));
-    c.addNote(new Note(1, Note.Pitch.A, 1,5));
+    c.addNote(new Note(1, Note.Pitch.A, 1, 5));
     c.addNote(new Note(2, Note.Pitch.G, 1, 5));
-    c.addNote(new Note(4, Note.Pitch.B, 1,5));
-    c.addNote(new Note(5, Note.Pitch.A, 1,5));
-    c.addNote(new Note(6, Note.Pitch.G, 1, 5));
+    c.addNote(new Note(3, Note.Pitch.B, 1, 5));
+    c.addNote(new Note(4, Note.Pitch.A, 1, 5));
+    c.addNote(new Note(5, Note.Pitch.G, 1, 5));
     m.playComposition(c);
   }
 }
