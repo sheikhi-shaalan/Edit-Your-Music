e9f2109d540c7e82e143383b6498abccf116b587
baharsheikhi <haji-sheikhi.b@husky.neu.edu>
2016-06-16 21:26:04 -0400

    Merge branch 'master' of https://github.com/baharsheikhi/hw06 into bahar6/13/16
    # Conflicts:
    #	src/cs3500/music/view/MidiViewImpl.java

--- src/cs3500/music/MusicEditor.java
+++ src/cs3500/music/MusicEditor.java
@@ -1,6 +1,9 @@
 package cs3500.music;
 
+import cs3500.music.model.MusicCreator;
 import cs3500.music.model.MusicCreatorImpl;
+import cs3500.music.model.Note;
+import cs3500.music.view.ConcreteGuiViewPanel;
 import cs3500.music.view.GuiViewFrame;
 import cs3500.music.view.MidiViewImpl;
 
@@ -10,7 +13,10 @@ import javax.sound.midi.InvalidMidiDataException;
 
 public class MusicEditor {
   public static void main(String[] args) throws IOException, InvalidMidiDataException {
-    GuiViewFrame view = new GuiViewFrame(new MusicCreatorImpl());
+    MusicCreator c = new MusicCreatorImpl();
+    c.addNote(new Note(0, Note.Pitch.C, 3, 4));
+      ConcreteGuiViewPanel s = new ConcreteGuiViewPanel(c);
+    GuiViewFrame view = new GuiViewFrame(c);
     //MidiView midiView = new MidiViewImpl();
     // You probably need to connect these views to your model, too...
   }
